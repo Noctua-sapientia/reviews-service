596917f28777aa40aa427140a6f6a91f57752093
Paula Yuan César Aguilar <paucesagu@alum.us.es>
2023-12-13 19:53:53 +0100

    new try Okteto

--- app.js
+++ app.js
@@ -17,4 +17,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/api/v1/reviews/books', bookReviewsRouter);
 
+//conexion con la base de datos
+/*const mongoose = require('mongoose');
+const DB_URL = (process.env.DB_URL || 'mongodb://localhost/test');
+
+mongoose.connect(DB_URL);
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'db connection error'));*/
+
 module.exports = app;
