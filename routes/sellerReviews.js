3efed9b427e9d4d4f7b10da9657e89d9c574434c
joscalval2 <joscalval2@alum.us.es>
2024-01-24 12:48:49 +0100

    fix: Checking if an order exists

--- routes/sellerReviews.js
+++ routes/sellerReviews.js
@@ -109,10 +109,10 @@ router.post('/', validateJWT, rateLimitMiddleware, async function(req, res, next
   const accessToken = req.headers.authorization;
   const {sellerId, customerId, description, rating} = req.body;
 
-  /*const resExistsOrder = await Order.existsOrder(sellerId, customerId,accessToken);
+  const resExistsOrder = await Order.existsOrder(sellerId, customerId,accessToken);
   if (resExistsOrder === null) { return res.status(502).send("There is a problem in orders microservice"); }
   if ( !resExistsOrder ) { return res.status(400).send("The user have not done any order to that seller, so he must not rate him"); }
-*/
+
   if ( !validateRating(rating) ) { return res.status(400).send("Rating must be a number between 1 and 5."); }
 
   try {
